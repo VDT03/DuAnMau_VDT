70623922eb17e1bfe52a3b30e15ca7c4b6b44477
Vu Duc Tai <vutai0122@gmail.com>
2024-03-20 12:28:27 +0700

    sua db

--- commons/env.php
+++ commons/env.php
@@ -11,6 +11,6 @@ define('DB_HOST','localhost');
 define('DB_PORT','3306');
 define('DB_USERNAME','root');
 define('DB_PASSWORD','');
-define('DB_NAME','duan1');
+define('DB_NAME','duan1_vdt');
 
 ?>
